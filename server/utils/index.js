986267f00c705186e9306aa406cc18633b3ea384
RAHUL K P <rahulkpkurup@gmail.com>
2026-07-26 22:01:43 +0530

    feat: add JWT creation utility

---
+++ server/utils/index.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+const createJWT = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  });
+
+  return token;
+};
+
+export default createJWT;
